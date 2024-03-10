d2edf5e6de3232693e2e6e122c42b0790c0b0467
Nabih <nabih@basma.com>
2024-03-10 10:34:03 +0200

    DB fix

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const db = CyclicDb("long-lime-mussel-garbCyclicDB")
 app.use(express.json());
 
 app.post('/vote/:number', async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     let responseJson = {};
     let votes = db.collection('votes')
     const { number } = req.params;
@@ -22,6 +23,7 @@ app.post('/vote/:number', async (req, res) => {
 });
 
 app.get('/vote/:number', async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
 	let responseJson = {};
     let votes = db.collection('votes')
     const { number } = req.params;
@@ -38,6 +40,7 @@ app.get('/vote/:number', async (req, res) => {
 });
 
 app.delete('/vote/:number', async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
 	let responseJson = {};
     let votes = db.collection('votes')
     const { number } = req.params;

5bb713f3ac70bed0e3cc1ff3e2512cbdd0ae0148
Nabih <nabih@basma.com>
2024-04-13 10:47:12 +0300

    Testing

--- index.js
+++ index.js
@@ -69,6 +69,9 @@ app.get('/votes/:number', async (req, res) => {
 
     let item = await votes.item("votes").fragment(number).get();
     console.log("item", item);
+    console.log("item", item[0]);
+    console.log("item", item[0].props);
+    console.log("item", item[0].props.vote);
     if (item != null && item.props.vote == 1) {
         responseJson.status = "voted";
     } else {

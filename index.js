99f04ed1583e6132437882f73ec7be9b7b74c413
Nabih <nabih.ahmad@hotmail.com>
2024-04-14 04:04:02 +0300

    v2 backend

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 const app = express();
 app.use(express.json())
 app.use(cors({
-    origin: 'https://oea-vote.netlify.app',
+    origin: ['https://oea-vote.netlify.app', 'https://oea-search.netlify.app', 'https://oea-admin.netlify.app'],
+    // origin: ['http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003'],
 }));
 require('dotenv').config();
 const CyclicDb = require("cyclic-dynamodb")
 const db = CyclicDb("long-lime-mussel-garbCyclicDB")
+var { Parser } = require('json2csv')
 
 app.post('/vote/:number', async (req, res) => {
     let responseJson = {};
@@ -51,21 +53,28 @@ app.get('/vote/:number', async (req, res) => {
 });
 
 app.get('/votes', async (req, res) => {
-	let responseJson = {};
+    let responseJson = [];
     let votes = db.collection('votes');
+    let votesItem = await votes.item("votes").fragments();
+    for (var i = 0; i < votesItem.length; i++) {
+        responseJson[i] = {"id": votesItem[i]};
+    }
 
-    let votesItem = await votes.item("votes").get();
-    console.log("votesItem", votesItem);
+    const fields = [{
+        label: 'OEA ID',
+        value: 'id'
+    }]
 
-    let votesItem2 = await votes.item("votes");
-    console.log("votesItem2", votesItem2);
+    const json2csv = new Parser({ fields: fields })
 
-    let votesItem3 = await votes.get("votes");
-    console.log("votesItem3", votesItem3);
-    
-    responseJson.status = votesItem;
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify(responseJson));
+    try {
+        const csv = json2csv.parse(responseJson)
+        res.attachment('votes.csv')
+        res.status(200).send(csv)
+    } catch (error) {
+        console.log('error:', error.message)
+        res.status(500).send(error.message)
+    }
 });
 
 app.get('/votes/:number', async (req, res) => {
@@ -90,7 +99,7 @@ app.delete('/vote/:number', async (req, res) => {
     const { number } = req.params;
 
     let item = await votes.set(number, {vote:0});
-    if (item != null) {
+    if (item != null) { d
         responseJson.status = "removed vote";
     } else {
         responseJson.status = "didn't remove vote";
@@ -104,8 +113,144 @@ app.delete('/votes/:number', async (req, res) => {
     let votes = db.collection('votes');
     const { number } = req.params;
 
-    await votes.item("votes").fragment(number).set({vote:0});
+    await votes.item("votes").fragment(number).delete();
     responseJson.status = "removed vote";
+	
+    res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(responseJson));
+});
+
+app.post('/register/:number', async (req, res) => {
+    let responseJson = {};
+    let votes = db.collection('votes');
+    const { number } = req.params;
+
+    await votes.item("registered").fragment(number).set({registered:1});
+
+    responseJson.status = "success";
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(responseJson));
+});
+
+app.get('/register', async (req, res) => {
+    let responseJson = [];
+    let votes = db.collection('votes');
+    let registeredItem = await votes.item("registered").fragments();
+    for (var i = 0; i < registeredItem.length; i++) {
+        responseJson[i] = {"id": registeredItem[i]};
+    }
+
+    const fields = [{
+        label: 'OEA ID',
+        value: 'id'
+    }]
+
+    const json2csv = new Parser({ fields: fields })
+
+    try {
+        const csv = json2csv.parse(responseJson)
+        res.attachment('registered.csv')
+        res.status(200).send(csv)
+    } catch (error) {
+        console.log('error:', error.message)
+        res.status(500).send(error.message)
+    }
+});
+
+app.get('/register/:number', async (req, res) => {
+	let responseJson = {};
+    let votes = db.collection('votes');
+    const { number } = req.params;
+
+    let item = await votes.item("registered").fragment(number).get();
+    console.log("item", item);
+    if (item[0] != null && item[0].props.registered == 1) {
+        responseJson.status = "registered";
+    } else {
+        responseJson.status = "not registered";
+    }
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(responseJson));
+});
+
+app.delete('/register/:number', async (req, res) => {
+    let responseJson = {};
+    let votes = db.collection('votes');
+    const { number } = req.params;
+
+    await votes.item("registered").fragment(number).delete();
+    responseJson.status = "removed registered engineer";
+	
+    res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(responseJson));
+});
+
+app.post('/mobile/:number', async (req, res) => {
+    let responseJson = {};
+    let votes = db.collection('votes');
+    const { number } = req.params;
+    let mobile = req.body.mobile;
+    console.log("mobile", mobile);
+
+    await votes.item("mobile").fragment(number).set({"mobile":mobile});
+
+    responseJson.status = "success";
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(responseJson));
+});
+
+app.get('/mobile', async (req, res) => {
+    let responseJson = [];
+    let votes = db.collection('votes');
+    let mobileItem = await votes.item("mobile").fragments();
+    for (var i = 0; i < mobileItem.length; i++) {
+        responseJson[i] = {"id": mobileItem[i], "mobile": mobileItem[i]};
+    }
+
+    const fields = [{
+        label: 'OEA ID',
+        value: 'id'
+    },{
+        label: 'Mobile',
+        value: 'mobile'
+    }]
+
+    const json2csv = new Parser({ fields: fields })
+
+    try {
+        const csv = json2csv.parse(responseJson)
+        res.attachment('mobile.csv')
+        res.status(200).send(csv)
+    } catch (error) {
+        console.log('error:', error.message)
+        res.status(500).send(error.message)
+    }
+});
+
+app.get('/mobile/:number', async (req, res) => {
+	let responseJson = {};
+    let votes = db.collection('votes');
+    const { number } = req.params;
+
+    let item = await votes.item("mobile").fragment(number).get();
+    console.log("item", item);
+    if (item[0] != null && item[0].props.mobile != null) {
+        responseJson.id = number;
+        responseJson.status = item[0].props.mobile;
+    } else {
+        responseJson.status = "not updated";
+    }
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(responseJson));
+});
+
+app.delete('/mobile/:number', async (req, res) => {
+    let responseJson = {};
+    let votes = db.collection('votes');
+    const { number } = req.params;
+
+    await votes.item("mobile").fragment(number).delete();
+    responseJson.status = "removed mobile";
 	
     res.setHeader('Content-Type', 'application/json');
 	res.send(JSON.stringify(responseJson));
@@ -113,5 +258,5 @@ app.delete('/votes/:number', async (req, res) => {
 
 // Start the server
 app.listen(3000, () => {
-  console.log(`Server running at http://localhost:3000`);
+    console.log(`Server running at http://localhost:3000`);
 });
